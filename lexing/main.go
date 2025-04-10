4f4b73e71216f79b4ee278c4a70f07771bb2a833
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-10 03:05:08 -0700

    lexing

--- lexing/main.go
+++ lexing/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/diegopacheco/writing-interpreter-in-go/repl"
+)
 
 func main() {
-	fmt.Println("hello world")
+	user, err := user.Current()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
