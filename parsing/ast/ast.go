b4ee22b982e3dd5e86b6756b534e3d71b7432bfa
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-23 08:02:00 -0700

    keep improving parser/ast

--- parsing/ast/ast.go
+++ parsing/ast/ast.go
@@ -255,3 +255,25 @@ type FunctionaLiteral struct {
 	Parameters []*Identifier
 	Body       *BlockStatement
 }
+
+func (fl *FunctionaLiteral) expressionNode() {}
+
+func (fl *FunctionaLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionaLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+	return out.String()
+}
