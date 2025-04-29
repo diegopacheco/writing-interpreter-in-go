1659a815037913ca54f4d07e26eef3ce8ec9f8f7
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-28 20:12:15 -0700

    improve the lang - improve repl by calling evaluator

---
+++ objectsystem/repl/repl.go
@@ -0,0 +1,60 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/diegopacheco/writing-interpreter-in-go/evaluator"
+	"github.com/diegopacheco/writing-interpreter-in-go/lexer"
+	"github.com/diegopacheco/writing-interpreter-in-go/parser"
+)
+
+const MONKEY_FACE = `
+           ."\"".
+       .-./ _=_ \.-.
+      {  (,(oYo),) }}
+      {{ |   "   |} }
+      { { \(---)/  }}
+      {{  }'-=-'{ } }
+      { { }._:_.{  }}
+      {{  } -:- { } }
+      {_{ }'==='{  _}
+     ((((\)     (/))))
+`
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
