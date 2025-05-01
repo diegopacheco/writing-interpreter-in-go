23ea8bdfa6afa7defbb50189bf15372e6f7c1e84
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-30 22:42:17 -0700

    Improve the lang - evaluator

--- objectsystem/object/object.go
+++ objectsystem/object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/diegopacheco/writing-interpreter-in-go/objectsystem/ast"
+)
 
 const (
 	INTEGER_OBJ      = "INTEGER"
@@ -8,6 +14,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type ObjectType string
@@ -17,59 +24,51 @@ type Object interface {
 	Inspect() string
 }
 
-type Integer struct {
-	Value int64
-}
+type Integer struct{ Value int64 }
 
-func (i *Integer) Inspect() string {
-	return fmt.Sprintf("%d", i.Value)
-}
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
-func (i *Integer) Type() ObjectType {
-	return INTEGER_OBJ
-}
+type Boolean struct{ Value bool }
 
-type Boolean struct {
-	Value bool
-}
-
-func (b *Boolean) Inspect() string {
-	return fmt.Sprintf("%t", b.Value)
-}
-
-func (b *Boolean) Type() ObjectType {
-	return BOOLEAN_OBJ
-}
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
 
-func (n *Null) Inspect() string {
-	return "null"
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
+
+type ReturnValue struct{ Value Object }
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+
+type Error struct{ Message string }
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
 
-func (n *Null) Type() ObjectType {
-	return NULL_OBJ
-}
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
 
-type ReturnValue struct {
-	Value Object
-}
+	params := make([]string, len(f.Parameters))
+	for i, p := range f.Parameters {
+		params[i] = p.String()
+	}
 
-func (rv *ReturnValue) Inspect() string {
-	return rv.Value.Inspect()
-}
+	out.WriteString("fn(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
 
-func (rv *ReturnValue) Type() ObjectType {
-	return RETURN_VALUE_OBJ
-}
-
-type Error struct {
-	Message string
-}
-
-func (e *Error) Inspect() string {
-	return fmt.Sprintf("ERROR: %s", e.Message)
-}
-func (e *Error) Type() ObjectType {
-	return ERROR_OBJ
+	return out.String()
 }
