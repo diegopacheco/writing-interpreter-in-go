40f75aa561b239ec60c268a4d79f51b3b0421a0a
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-30 22:41:04 -0700

    Improve the lang - evaluator

--- objectsystem/ast/ast.go
+++ objectsystem/ast/ast.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"strings"
 
-	"github.com/diegopacheco/writing-interpreter-in-go/token"
+	"github.com/diegopacheco/writing-interpreter-in-go/objectsystem/token"
 )
 
 type Node interface {
@@ -250,34 +250,6 @@ func (bs *BlockStatement) String() string {
 	return out.String()
 }
 
-type FunctionaLiteral struct {
-	Token      token.Token
-	Parameters []*Identifier
-	Body       *BlockStatement
-}
-
-func (fl *FunctionaLiteral) expressionNode() {}
-
-func (fl *FunctionaLiteral) TokenLiteral() string {
-	return fl.Token.Literal
-}
-
-func (fl *FunctionaLiteral) String() string {
-	var out bytes.Buffer
-
-	params := []string{}
-	for _, p := range fl.Parameters {
-		params = append(params, p.String())
-	}
-
-	out.WriteString(fl.TokenLiteral())
-	out.WriteString("(")
-	out.WriteString(strings.Join(params, ", "))
-	out.WriteString(") ")
-	out.WriteString(fl.Body.String())
-	return out.String()
-}
-
 type CallExpression struct {
 	Token     token.Token
 	Function  Expression
@@ -302,5 +274,33 @@ func (ce *CallExpression) String() string {
 	out.WriteString("(")
 	out.WriteString(strings.Join(args, ", "))
 	out.WriteString(")")
+	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 	return out.String()
 }
