525db802076c60bd493e005270e9db98e0c3dbc4
diegopacheco <diego.pacheco.it@gmail.com>
2025-04-30 20:02:07 -0700

    Improve the lang - evaluator

--- objectsystem/evaluator/evaluator_test.go
+++ objectsystem/evaluator/evaluator_test.go
@@ -41,7 +41,9 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
-	return Eval(program)
+	env := object.NewEnvironment()
+
+	return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T,
@@ -182,6 +184,7 @@ func TestErrorHandling(t *testing.T) {
 		{"-true", "unknown operator: -BOOLEAN"},
 		{"true + false;", "type mismatch: BOOLEAN + BOOLEAN"},
 		{"5; true + false; 5", "type mismatch: BOOLEAN + BOOLEAN"},
+		{"foobar", "identifier not found: foobar"},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
@@ -192,6 +195,27 @@ func TestErrorHandling(t *testing.T) {
 		}
 		if errObj.Message != tt.expected {
 			t.Errorf("wrong error message. expected=%q, got=%q", tt.expected, errObj.Message)
+		}
+	}
+}
+
+func TestLetStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = b + 5; c;", 10},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
 		}
 	}
 }
