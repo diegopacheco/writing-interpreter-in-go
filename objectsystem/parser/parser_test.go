36b671240d040f0f3a500a61ac9362452f664315
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-02 06:42:23 -0700

    Improve the lang - evaluator

--- objectsystem/parser/parser_test.go
+++ objectsystem/parser/parser_test.go
@@ -725,5 +725,25 @@ func TestStringLiteralExpression(t *testing.T) {
 	if literal.TokenLiteral() != "Hello World" {
 		t.Errorf("literal.TokenLiteral() wrong. got=%q", literal.TokenLiteral())
 	}
+}
 
+func TestParsingArrayLiteral(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	stmt, _ := program.Statements[0].(*ast.ExpressionStatement)
+	array, ok := stmt.Expression.(*ast.ArrayLiteral)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+	if len(array.Elements) != 3 {
+		t.Fatalf("array.Elements wrong. got=%d", len(array.Elements))
+	}
+	testIntegerLiteral(t, array.Elements[0], 1)
+	testInfixExpresson(t, array.Elements[1], 2, "*", 2)
+	testInfixExpresson(t, array.Elements[2], 3, "+", 3)
 }
