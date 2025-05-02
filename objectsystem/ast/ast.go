8016b2fddf8bbce62c0dd80a9779f57cd8c4c84f
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-01 19:35:24 -0700

    Improve the lang - evaluator

--- objectsystem/ast/ast.go
+++ objectsystem/ast/ast.go
@@ -304,3 +304,18 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString(fl.Body.String())
 	return out.String()
 }
+
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode() {}
+
+func (sl *StringLiteral) TokenLiteral() string {
+	return sl.Token.Literal
+}
+
+func (sl *StringLiteral) String() string {
+	return sl.Token.Literal
+}
