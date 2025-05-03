d27c036d428afae907cbc47254c5956873fce300
diegopacheco <diego.pacheco.it@gmail.com>
2025-05-02 17:49:03 -0700

    Improve the lang - evaluator

--- objectsystem/object/object.go
+++ objectsystem/object/object.go
@@ -17,6 +17,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 type ObjectType string
@@ -95,4 +96,24 @@ func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 
 func (b *Builtin) Inspect() string {
 	return "builtin function"
+}
+
+type Array struct {
+	Elements []Object
+}
+
+func (a *Array) Type() ObjectType { return ARRAY_OBJ }
+
+func (a *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := make([]string, len(a.Elements))
+	for i, e := range a.Elements {
+		elements[i] = e.Inspect()
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+	return out.String()
 }
